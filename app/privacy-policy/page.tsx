9fb7dd0f91168cd8551ea5538f9cb3fae6d3b71c
Faez <faez@xmaagency.com>
2025-01-30 21:20:06 +0400

    making privacy policy look nice

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Container, Section } from "@radix-ui/themes";
+import React from "react";
 
 function page() {
   return (
-    <div><h1>Privacy Policy</h1></div>
-  )
+      <Section className="container mb-2xl mx-auto">
+      <div className="pt-28 h-screen">
+        <h1>Privacy Policy</h1>
+      </div>
+    </Section>
+  );
 }
 
-export default page
+export default page;
