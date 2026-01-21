31eebf890ddfb1b230602f2e42b529f982782501
Faez <faez@xmaagency.com>
2026-01-21 20:52:26 +0400

    ISR BrowserShowcaseSection

--- app/(website)/(main)/_components/BrowserShowcaseSection/index.tsx
+++ app/(website)/(main)/_components/BrowserShowcaseSection/index.tsx
@@ -1,9 +1,16 @@
+import { unstable_cache } from 'next/cache'
 import { getFeaturedShowcaseWebsites } from '@/sanity/lib/showcase'
 import Section from '@/components/ui/section'
 import BrowserShowcaseClient from './BrowserShowcaseClient'
 
+const getCachedShowcaseWebsites = unstable_cache(
+  async () => getFeaturedShowcaseWebsites(4),
+  ['showcase-websites'],
+  { revalidate: 3600 }
+)
+
 const BrowserShowcaseSection = async () => {
-  const websites = await getFeaturedShowcaseWebsites(4)
+  const websites = await getCachedShowcaseWebsites()
 
   if (websites.length === 0) return null
 
